b3d019496c04592ca3e979757be960bb4daefcb9
kevincyubahiro <kevincyubahiro250@gmail.com>
2024-09-20 03:31:43 -0700

    hello

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import'./app.css'
+import Mom from './component'
+function app(){
+  return(
+    <>
+    
+    <Mom/>
+    </>
+  )
+}
+export default app;
---
+++ component.jsx
@@ -0,0 +1,7 @@
+const mynames='kevinpro'
+const Mom=()=>{
+    return(
+        <h3>hello my people</h3>
+    )
+}
+export default Mom;
